9fbea71c334d8b3c01a33754a0c38b2c8f5ab1be
emilycharris <emily.harris.mail@gmail.com>
2016-07-26 22:37:49 -0400

    added heroku database stuff

--- what_to_watch/settings.py
+++ what_to_watch/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -89,7 +91,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
